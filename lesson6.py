8f8b06a1350e36bdcf668d7cbff42be0faf7f9f0
jtnugent <nugentj@wlhs.wlwv.k12.or.us>
2025-03-31 09:48:42 -0700

    initial commit

---
+++ lesson6.py
@@ -0,0 +1,7 @@
+from PIL import Image, ImageFilter, ImageEnhance
+
+orange_cat = Image.open(r"orange_cat.jpg")
+flipped_orange_cat = orange_cat.transpose(Image.FLIP_TOP_BOTTOM)
+flipped_orange_cat.show()
+rotated_cat = orange_cat.rotate(45)
+rotated_cat.show()

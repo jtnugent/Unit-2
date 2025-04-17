0ee021c8c50ce3b5179af15262c0ce6334fb6d08
jtnugent <nugentj@wlhs.wlwv.k12.or.us>
2025-04-17 20:00:10 +0000

    Running Loop

--- TAKE_HOME_FINAL.py
+++ TAKE_HOME_FINAL.py
@@ -25,7 +25,7 @@ class Plant:
     def menu(self):
         print(f"Actions Left: {self.action}    Day: {self.day}")
         print(f"Water: {self.water}%    Evo: {self.evo}%    Hunger: {self.hunger}%")
-        print("1 - water\n2 - feed\n3 - trim\n4 - wait")
+        print("1 - water (+50 water)\n2 - feed (+50 hunger)\n3 - trim (-50 evo)\n4 - wait")
         action = int(input("Choose.\n"))
         if action == 1:
             self.water = self.water+50
@@ -36,27 +36,39 @@ class Plant:
         elif action == 4:
             pass
         self.action = self.action - 1
-        if action <= 0:
-            return
-
+        if self.action <= 0:
+            self.day_reset()
+        
+    def day_reset(self):
+        self.set_day(1)
+        if self.plant_choice == "tree":
+            Tree.reset(self)
+        elif self.plant_choice == "fern":
+            Fern.reset(self)
+        elif self.plant_choice == "succulent":
+            Succulent.reset(self)
+        self.action = 2
 
     def __repr__(self):
-        #acts as stats
-        return self.end
+        #Checks if lost
+        return f"{self.end}"
     
-    def __add__(self):
-        #Adds number to keep track of days.
-        self.day +=1
+    def get_day(self):
+        return self.day
+    
+    def set_day(self,x):
+        self.day = self.day + x
 
     def stat_check(self):
+        #If any stats are too large or too small it sends the info to the subclasses to get the correct message and image. 
         if self.hunger <=0 or self.hunger > 100 or self.water <= 0 or self.water > 100 or self.evo <=0 or self.evo >= 100:
             if self.plant_choice == "tree":
                 Tree(self.hunger, self.evo, self.water).stat_check()
             if self.plant_choice == "succulent":
                 Succulent(self.hunger, self.evo, self.water).stat_check()
-            if self.plant_choice == "Fern":
+            if self.plant_choice == "fern":
                 Fern(self.hunger, self.evo, self.water).stat_check()
-
+            self.end = True
 
 
 class Fern(Plant):
@@ -85,6 +97,11 @@ class Fern(Plant):
             print("overwatered")
             self.end = True
 
+    def reset(self):
+        self.hunger = self.hunger-10
+        self.water = self.water-40
+        self.evo = self.evo+20
+
 class Succulent(Plant):
     def __init__(self, hunger, evo, water):
         self.hunger = hunger
@@ -110,6 +127,11 @@ class Succulent(Plant):
         elif self.water > 100:
             print("overwatered")
             self.end = True
+
+    def reset(self):
+        self.hunger = self.hunger-10
+        self.water = self.water-10
+        self.evo = self.evo+30
             
 class Tree(Plant):
     def __init__(self, hunger, evo, water):
@@ -137,13 +159,38 @@ class Tree(Plant):
             print("overwatered")
             self.end = True
 
+    def reset(self):
+        self.hunger = self.hunger-20
+        self.water = self.water-30
+        self.evo = self.evo+40
+
 def main():
+
+    print(""" 
+                                  
+                            
+                             
+                              
+                            
+                              
+                           
+                              
+                            
+                      
+        |---------------|                                     
+        |               |                            
+        |               |          |        |          |-------------------|                                 
+        _________________          |________|          |___________________|      
+""")
     plant_choice = str.lower(input("Would you like to choose a Fern, Tree, or Succulent?\n"))
     plant = Plant(plant_choice, 50, 50, 2, 50)
     end = False
     while end == False:
         plant.menu()
         plant.stat_check()
-        if plant is True:
+
+        if repr(plant) == "True":
+            print("ending supposed")
             end = True
+    
 main()

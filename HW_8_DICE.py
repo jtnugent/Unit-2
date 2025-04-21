227d4b1fc96588805c7349b2e5c64a72ff3ef224
jtnugent <nugentj@wlhs.wlwv.k12.or.us>
2025-04-21 09:01:05 -0700

    commit

--- HW_8_DICE.py
+++ HW_8_DICE.py
@@ -7,37 +7,44 @@ class Die:
         print(random.randint(1,self.sides))
 
 class Ticket:
-    def __init__(self, lucky_ticket, guess_list, counter = 0):
-        self.guess_list = guess_list
-        self.lucky_ticket = lucky_ticket
+    def __init__(self, lucky_ticket, counter = 0):
         self.counter = counter
+        self.lucky_ticket = lucky_ticket
 
     def create_ticket(self):
         possible_list = [1,2,3,4,5,6,7,8,9,10,"a","b","c","d","e"]
-        for i in range(2):
+        self.guess_list = []
+        for i in range(1):
             self.guess_list.append(random.choice(possible_list))
         print(self.guess_list)
         print()
         self.counter += 1
-        return self.guess_list
+        return self.counter
 
     def check_ticket(self):
+        global winstate
+        print("start")
+        print(self.guess_list)
+        print(self.lucky_ticket)
+        print("end")
         if self.guess_list == self.lucky_ticket:
-            return False
+            return True
 
     def win(self):
-        pass
+        return self.counter
 
 
 def main():
+    global winstate
+    winstate = False
     possible_list = [1,2,3,4,5,6,7,8,9,10,"a","b","c","d","e"]
     lucky_list = []
-    for i in range(2):
+    for i in range(1):
         lucky_list.append(possible_list[random.randint(1,len(possible_list)-1)])
     print(f"Any ticket matching {lucky_list} WINS!")
     Win = True
-    ticket_checker = Ticket(lucky_list,[])
-    while Win:
+    ticket_checker = Ticket(lucky_list)
+    for i in range(1000):
         ticket_checker.create_ticket()
         ticket_checker.check_ticket()
         if ticket_checker.check_ticket():
